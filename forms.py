b682bda462fcf2ce7916f663302fcdf26379308b
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-01 05:31:57 +0300

    Translate new task form labels into Russian

--- forms.py
+++ forms.py
@@ -1,11 +1,12 @@
+#coding: utf-8
+
 from flask_wtf import Form
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired, Email
 
 
 class NewTaskForm(Form):
-    # TODO: translate labels to Russian
-    dest_address = StringField("Destination address", validators=[DataRequired(), Email()])
+    dest_address = StringField(u"Адрес назначения", validators=[DataRequired(), Email()])
 
-    message_subject = StringField("Message subject", validators=[DataRequired()])
-    message_text = TextAreaField("Message text", validators=[DataRequired()])
+    message_subject = StringField(u"Тема сообщения", validators=[DataRequired()])
+    message_text = TextAreaField(u"Текст сообщения", validators=[DataRequired()])
