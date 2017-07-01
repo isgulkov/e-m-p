ab275c51814656d5d3289901331e07b7d80b6c50
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-01 03:45:35 +0300

    Change single quotes to double where appropriate

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'TY PIDOR!'
+    return "TY PIDOR!"
 
 
 @app.errorhandler(500)
 def server_error(e):
-    logging.exception('An error occurred during a request: %s' % e)
-    return 'An internal error occurred.', 500
+    logging.exception("An error occurred during a request: %s" % e)
+    return "An internal error occurred.", 500
