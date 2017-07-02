b388295df9d4eae28fbb7a19b52f1b508ef31fa3
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-02 20:27:02 +0300

    Add db settings into config.py

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
 DEBUG = True
 SECRET_KEY = 'DEVELOPMENT_SECRET_KEY' # change to a cryptographic random string in production (lolkek)
+
+CLOUDSQL_CONNECTION_NAME = 'viagranedorogo:europe-west1:viagra-mysql-db'
+CLOUDSQL_USER = 'root'
+CLOUDSQL_PASSWORD = 'cCfasoKtneAHnjbP'
+CLOUDSQL_DBNAME = 'viagra'
