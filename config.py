ab16c31981e465c7b9ebe6b8f623adf964e20cb3
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-02 20:36:26 +0300

    Replace config.py in the repo with an example config

--- config.py
+++
@@ -1,7 +0,0 @@
-DEBUG = True
-SECRET_KEY = 'DEVELOPMENT_SECRET_KEY' # change to a cryptographic random string in production (lolkek)
-
-CLOUDSQL_CONNECTION_NAME = 'viagranedorogo:europe-west1:viagra-mysql-db'
-CLOUDSQL_USER = 'root'
-CLOUDSQL_PASSWORD = 'cCfasoKtneAHnjbP'
-CLOUDSQL_DBNAME = 'viagra'
