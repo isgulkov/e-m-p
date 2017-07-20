83f131b5c118fc21ff10273a5f6ef759fe37b694
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-20 18:06:28 +0300

    Add stub for mailjet API wrapper

---
+++ emails.py
@@ -0,0 +1,11 @@
+from mailjet_rest import Client
+
+
+class Emails:
+    @staticmethod
+    def send_email(recipient, subject, html):
+        pass
+
+    @staticmethod
+    def process_notify(ne_yebu_poka):
+        pass
