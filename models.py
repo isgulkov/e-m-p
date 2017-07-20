98534a8bf34de37e0a17e9dfae9a76a80e3dbb1a
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-20 23:08:11 +0300

    Add Email model

--- models.py
+++ models.py
@@ -1,6 +1,16 @@
-from sqlalchemy import Column, Integer, String, Text, Enum
+from sqlalchemy import Column, ForeignKey, Integer, String, Text, Enum, DateTime
+from sqlalchemy.orm import relationship
 
 from database import Base
+
+
+def trunc_content(s):
+    s.replace('\r', '').replace('\n', '')
+
+    if len(s) > 25:
+        s = s[:25] + "..."
+
+    return s
 
 
 class EmailTask(Base):
@@ -15,16 +25,35 @@ class EmailTask(Base):
 
     status = Column(Enum('SCHEDULED', 'IN_PROGRESS', 'FAILED', 'COMPLETED', name='emailtask_status'), default='SCHEDULED')
 
+    emails = relationship('Email', backref='task')
+
     def __repr__(self):
-        def trunc_content(s):
-            s.replace('\r', '').replace('\n', '')
-
-            if len(s) > 25:
-                s = s[:25] + "..."
-
-            return s
-
         return "[Task with status %s to message '%s' with subject '%s' and content '%s']" % (
+            self.status,
+            self.dest_address,
+            self.message_subject,
+            trunc_content(self.message_content),
+        )
+
+class Email(Base):
+    __tablename__ = 'emails'
+
+    id = Column(Integer, primary_key=True)
+
+    dest_address = Column(String(256))
+
+    subject = Column(Text)
+    content = Column(Text)
+
+    status = Column(Enum('SCHEDULED', 'SENT', 'OPENED', 'SPAM', name='email_status'), default='SCHEDULED')
+
+    sent_date = Column(DateTime)
+
+    task_id = Column(Integer, ForeignKey(EmailTask.id))
+
+    def __repr__(self):
+
+        return "[Task with status %s to %s with subject '%s' and content '%s']" % (
             self.status,
             self.dest_address,
             self.message_subject,
