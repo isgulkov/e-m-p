9bf92ef9aea54c44987a1cb083e55dbf256a865c
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-15 04:01:33 +0300

    Rename EmailTask into EmailJob everywhere
    To avoid confusion with gcloud tasks

--- init_db.py
+++ init_db.py
@@ -1,5 +1,5 @@
 from database import Base, engine
-from models import EmailTask, Email
+from models import EmailJob, Email
 
 Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
