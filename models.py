5eae4e8fdfa9f5034788a9ce06b000dbab8ee5ac
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-15 07:34:05 +0300

    Add a random unique id string to the Email model

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+from random import choice
+
 from sqlalchemy import Column, ForeignKey, Integer, String, Text, Enum, DateTime
 from sqlalchemy.orm import relationship
 
@@ -35,25 +37,46 @@ class EmailJob(Base):
             trunc_content(self.message_content),
         )
 
+
+class RandomB64StringGenerator:
+    def __init__(self):
+        self.charset = ['-', '_']
+
+        for a, b in (('a', 'z'), ('A', 'Z'), ('0', '9')):
+            for c in [chr(i) for i in xrange(ord(a), ord(b) + 1)]:
+                self.charset.append(c)
+
+    def get_random_string(self, length):
+        return ''.join([choice(self.charset) for _ in xrange(length)])
+
+
 class Email(Base):
     __tablename__ = 'emails'
 
     id = Column(Integer, primary_key=True)
+
+    uid = Column(String(32))
 
     dest_address = Column(String(256))
 
     subject = Column(Text)
     content = Column(Text)
 
-    status = Column(Enum('SENT', 'OPENED', 'SPAM', name='email_status'), default='SCHEDULED')
+    status = Column(Enum('ENQUEUED', 'SENT', 'OPENED', 'SPAM', name='email_status'), default='ENQUEUED')
 
     sent_date = Column(DateTime)
 
-    task_id = Column(Integer, ForeignKey(EmailJob.id))
+    job_id = Column(Integer, ForeignKey(EmailJob.id))
+
+    uidGen = RandomB64StringGenerator()
+
+    def __init__(self):
+        # Generate a random unique id to identify the email in notify URLs
+
+        self.uid = self.uidGen.get_random_string(32)
 
     def __repr__(self):
-
-        return "[Task with status %s to %s with subject '%s' and content '%s']" % (
+        return "[Email with status %s to %s with subject '%s' and content '%s']" % (
             self.status,
             self.dest_address,
             self.message_subject,
